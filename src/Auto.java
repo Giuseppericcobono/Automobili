89d453d0e04116dd5cb31c3027d5d9ce5085c038
Giuseppericcobono <giuseppe.riccobono@hotmail.com>
2024-04-05 11:36:44 +0200

    First Commit

---
+++ src/Auto.java
@@ -0,0 +1,58 @@
+public class Auto {
+
+    private String marca;
+
+    private String modello;
+
+    private String targa;
+
+    private String cilindrata;
+
+    public Auto(String marca, String modello, String targa, String cilindrata){
+        this.marca = marca;
+        this.modello = modello;
+        this.targa = targa;
+        this.cilindrata = cilindrata;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public String getTarga() {
+        return targa;
+    }
+
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+
+    public String getCilindrata() {
+        return cilindrata;
+    }
+
+    public void setCilindrata(String cilindrata) {
+        this.cilindrata = cilindrata;
+    }
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "marca='" + marca + '\'' +
+                ", modello='" + modello + '\'' +
+                ", targa='" + targa + '\'' +
+                ", cilindrata='" + cilindrata + '\'' +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto auto1 = new Auto("Fiat", "Punto", "CC194AB", "1200cc");
+        System.out.println(auto1);
+    }
+}
